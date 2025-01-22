61f64f8873afc7bdbd5e9f306b800367c69ce8df
d0meq <moutiie11@gmail.com>
2025-01-23 00:40:32 +0100

    Add server headers configuration in Vite for enhanced security

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/web-sudoku/"
+  base: "/web-sudoku/",
+  server: {
+    headers: {
+      "Permission-Policy": "",
+    }
+  }
 })
