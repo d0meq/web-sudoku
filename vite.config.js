4e4cc0abddf2e89effa8d158d44f7bed08b6b561
d0meq <moutiie11@gmail.com>
2025-01-23 00:45:30 +0100

    Update Vite configuration base path for web-sudoku deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/web-sudoku/",
   server: {
     headers: {
       "Permission-Policy": "",
